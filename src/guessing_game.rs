66965ea4636d6757bdaa0589fa550439989acc68
Muhammad Qasim <mqas2022@gmail.com>
2022-04-11 13:23:34 +0500

    done guessing

--- src/guessing_game.rs
+++ src/guessing_game.rs
@@ -1,20 +1,26 @@
 use rand::Rng;
 use std::{cmp::Ordering, io};
 
+#[allow(non_snake_case)]
 pub fn Runner() {
     println!("Welcome, Noob");
-    println!("Enter a guess number!!");
-
-    let mut guess = String::new();
-
     let sec_num = rand::thread_rng().gen_range(0..101);
+
     loop {
+        println!("Enter a guess number!!");
+
+        let mut guess = String::new();
+
         io::stdin()
             .read_line(&mut guess)
             .expect("Faild to get input");
 
+        let x: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
         // debug_line:: print!("The Guessed number is:: {}", guess)
-        let x: i32 = guess.trim().parse().expect("Expected a number!!");
 
         match x.cmp(&sec_num) {
             Ordering::Less => println!("Too Small"),
