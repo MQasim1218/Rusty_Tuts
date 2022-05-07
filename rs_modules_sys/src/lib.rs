01b3e77a5432acb71599aa70903909345c3f1854
Muhammad Qasim <mqas2022@gmail.com>
2022-05-07 21:44:16 +0500

    ...

---
+++ rs_modules_sys/src/lib.rs
@@ -0,0 +1,5 @@
+/*
+    This is the root file for any library written in Rust!!
+    The library has the same name as the package.. ie the name of the project.
+    Initialize library with 'Cargo new --lib lib-name'
+*/
