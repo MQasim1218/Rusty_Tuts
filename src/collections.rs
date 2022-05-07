38b13eaeda10bd32ee31cdc481c4989ccc33b78c
Muhammad Qasim <mqas2022@gmail.com>
2022-05-08 00:15:56 +0500

    some additions

--- src/collections.rs
+++ src/collections.rs
@@ -1,14 +1,74 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
+#![allow(unused_assignments)]
 
 mod vectors {
+    use std::vec;
 
     pub fn code() {
-        vects_vs_arys()
+        // vects_vs_arys();
+        // vect_indexes();
+        // iterating_vects();
+        single_value_types_workaround()
+    }
+
+    fn single_value_types_workaround() {
+        
+    }
+
+    fn iterating_vects() {
+        let mut vec1 = vec![3, 2, 5, 6, 8, 3, 9, 12];
+        vec1.push(17); // Pushing some more elements
+        vec1.remove(3); // Removing value at index 3
+
+        // Mutable iter
+        // Only use reference, dont borrow
+        println!("Iterating vectors -- Immutable ");
+        for i in &vec1 {
+            println!("value: {}", i)
+        }
+
+        // Immutable Iter
+        // Using a mutable reference this time!
+        println!("\nIterating vectors -- Mutable (+= 25)");
+        for i in &mut vec1 {
+            // * is the derefernce operator, more on this latrr!!
+            *i += 25;
+            println!("value: {}", i)
+        }
+    }
+
+    fn vect_indexes() {
+        /*
+            Two methods!!
+            Indexing with index number or use get method.
+        */
+        let mut vec1 = vec![1, 2, 3, 4, 5, 6];
+
+        /*
+            Getting the value from the vector!!
+            After getting the value, we can push to the vec as the value
+            taken is copied to the variable.
+        */
+        let third = vec1[2];
+        vec1.push(12);
+        println!("{:?}", third);
+
+        let sec = &vec1[1]; // Immutable borrow
+        vec1.push(22); // Mutable borrow!!
+                       // println!("This is the second value of the vector {}", sec);
+                       // Error as mut and immut borrow takes place.
+
+        // Indexing with get method
+        if let Some(val) = vec1.get(8) {
+            println!("The value at index 3 is {}", val);
+        } else {
+            println!("The index is incorrect!!");
+        }
     }
 
     fn vects_vs_arys() {
-        let ary1 = [1, 2, 3, 4, 5];
+        let mut ary1 = [1, 2, 3, 4, 5];
         let mut vec1: Vec<i32> = Vec::new();
         vec1.push(1);
         vec1.push(2);
@@ -16,8 +76,24 @@ mod vectors {
         vec1.push(4);
         vec1.push(5);
 
-        println!("{:?}", ary1);
-        println!("{:?}", vec1);
+        println!("Array: {:?}", ary1);
+        println!("Vector: {:?}", vec1);
+        println!();
+
+        ary1 = [12, 12, 15, 17, 12]; // The internal values can be changed but the length remains unchanged!!
+        ary1[4] = 23;
+        vec1[4] = 23;
+
+        println!("Array {:?}", ary1);
+        println!("Vector {:?}", vec1);
+        println!();
+
+        vec1.push(54);
+
+        println!("Vector can grow in length!!");
+        println!("Array {:?}", ary1);
+        println!("Vector {:?}", vec1);
+        println!();
     }
 }
 
